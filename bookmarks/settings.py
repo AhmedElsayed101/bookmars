dd0e5be017001572065a70dc3876cb29dfa5c49a
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-02-09 15:20:48 +0200

    Add account app and set some variables

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #local
+    'account.apps.AccountConfig',
+    #django
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd
+
+    
 ]
 
 MIDDLEWARE = [
@@ -118,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

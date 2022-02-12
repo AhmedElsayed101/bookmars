ef98877f55b653614d2b6a5c7dbfdf0c2585ae55
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-02-12 14:12:16 +0200

    Create edit view

--- account/views.py
+++ account/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .form import LoginForm, UserRegistrationFrom
+from .form import LoginForm, UserRegistrationFrom, UserEditForm, ProfileEditForm
+from .models import Profile
 
 
 def user_login(request):
@@ -57,6 +58,7 @@ def register(request):
                 user_form.cleaned_data['password']
             )
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(
                 request,
                 'account/register_done.html',
@@ -70,6 +72,31 @@ def register(request):
         request,
         'account/register.html',
         {
-            'user_form' : user_form
+            'user_form': user_form
+        }
+    )
+
+
+@login_required
+def edit(request):
+
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form' : user_form,
+            'profile_form' : profile_form
         }
     )

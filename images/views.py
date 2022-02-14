673c4aacef4f3782ebce0c31820fd8ac7e20cdf8
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-02-14 14:52:43 +0200

    Create like view

--- images/views.py
+++ images/views.py
@@ -1,14 +1,18 @@
+import json
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
 
 @login_required
 def image_create(request):
 
     if request.method == 'POST':
-        form = ImageCreateForm(data = request.POST)
+        form = ImageCreateForm(data=request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             new_item = form.save(commit=False)
@@ -17,17 +21,16 @@ def image_create(request):
             messages.success(request, 'Image added sucessfully')
             return redirect(new_item.get_absolute_url())
     else:
-        form = ImageCreateForm(data = request.GET)
+        form = ImageCreateForm(data=request.GET)
 
     return render(
         request,
         'images/image/create.html',
         {
             'section': 'images',
-            'form' : form
+            'form': form
         }
     )
-
 
 
 def image_detail(request, id, slug):
@@ -37,7 +40,24 @@ def image_detail(request, id, slug):
         request,
         'images/image/detail.html',
         {
-            'section' : 'images',
-            'image' : image
+            'section': 'images',
+            'image': image
         }
     )
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            return JsonResponse({'status': 'error'})

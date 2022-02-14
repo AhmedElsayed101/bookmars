ecd9152b946d88f9ad4d5a5e7c9bf7d0d51b7dde
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-02-14 13:34:57 +0200

    Add images urls and create image template

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('social-auth/', include('social_django.urls', namespace='social'),)
+    path('social-auth/', include('social_django.urls', namespace='social'),),
+    path('images/', include('images.urls', namespace='images'))
 ]
 
 

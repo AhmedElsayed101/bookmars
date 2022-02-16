9f1577377c0d02677d1d0b3e68ce254730b9f18e
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-02-16 16:58:51 +0200

    Add total like field

--- images/models.py
+++ images/models.py
@@ -3,11 +3,12 @@ from django.conf import settings
 from django.utils.text import slugify
 from django.urls import reverse
 
+
 class Image(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name='images_created', on_delete=models.CASCADE)
-    
+
     title = models.CharField(max_length=200)
     slug = models.CharField(max_length=200, blank=True)
     url = models.URLField()
@@ -19,15 +20,16 @@ class Image(models.Model):
         related_name='images_liked',
         blank=True
     )
+    total_likes = models.PositiveIntegerField(db_index=True, default=0)
 
     def __str__(self):
-        return self.title  
+        return self.title
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
-        super().save(*args, **kwargs)   
-    
+        super().save(*args, **kwargs)
+
     def get_absolute_url(self):
         return reverse(
             'images:detail',
